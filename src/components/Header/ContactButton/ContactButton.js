afae6340f7310ae01a4fae0789d67c1276ce346b
jeff brausam <jeffbrausam@jeffs-MacBook-Pro.local>
2021-03-06 19:07:08 -0800

    mobile edits

--- src/components/Header/ContactButton/ContactButton.js
+++ src/components/Header/ContactButton/ContactButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import sizing from '../../../styles/config/sizing';
+import breakpoint from '../../../styles/config/breakpoints';
 import Button from '../../UI/Button/Button';
 
 const StyledContactButton = styled(Button)`
@@ -8,7 +9,12 @@ const StyledContactButton = styled(Button)`
     right: 10%;
     top: ${sizing.size5};
     z-index: 200;
-    box-shadow: 1px 3px 5px rgba(0,0,0,.2); 
+    box-shadow: 1px 3px 5px rgba(0,0,0,.2);
+    
+  @media ${`(max-width: ${breakpoint.small})`} {
+    /* Top fix on mobile */
+    top: ${sizing.size2}
+  }
 `;
 
 const ContactButton = () => {
